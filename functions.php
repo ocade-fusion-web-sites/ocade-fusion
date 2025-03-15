7aa7785893e4fae74b02c946c8f860e3ade279f0
Valentin Charrier <valentin.charrier@hotmail.fr>
2025-03-15 17:55:18 +0100

    maj color

--- functions.php
+++ functions.php
@@ -7,3 +7,15 @@ function charger_prism() {
   wp_enqueue_script('prism-js', get_stylesheet_directory_uri() . '/prism/prism.js', [], null, true);
 }
 add_action('wp_enqueue_scripts', 'charger_prism');
+
+function personnalisation_gutenberg_colors() {
+  add_theme_support('editor-color-palette', [
+    ['name'  => __('Orange', 'mon-theme'), 'slug'  => 'orange', 'color' => '#ef4e39'],
+    ['name'  => __('Gris', 'mon-theme'), 'slug'  => 'gris', 'color' => '#f2f2f2'],
+    ['name'  => __('Gris Clair', 'mon-theme'), 'slug'  => 'gris-clair', 'color' => '#f4f4f4'],
+    ['name'  => __('Nuit', 'mon-theme'), 'slug'  => 'nuit', 'color' => '#32373c'],
+    ['name'  => __('Violet', 'mon-theme'), 'slug'  => 'violet', 'color' => '#303579']
+  ]);
+  add_theme_support('disable-custom-colors');
+}
+add_action('after_setup_theme', 'personnalisation_gutenberg_colors');
