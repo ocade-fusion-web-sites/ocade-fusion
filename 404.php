f850a6300e69254d2d3477b42d8ac312878e9ce8
Valentin Charrier <valentin.charrier@hotmail.fr>
2025-03-20 15:44:03 +0100

    CP GIT

---
+++ 404.php
@@ -0,0 +1,27 @@
+<?php
+
+get_header(); ?>
+
+<main role="main">
+
+  <?php
+  /** Récupération du CPT 404. */
+  $error_query = new WP_Query(array(
+    'post_type' => '404',
+    'post_status' => 'publish',
+    'posts_per_page' => 1,
+    'limit' => 1,
+    'orderby' => 'date',
+    'order' => 'DESC',
+  )) ?? null; ?>
+
+  <?php if ($error_query && $error_query->have_posts()) : ?>
+    <?php while ($error_query->have_posts()) : $error_query->the_post(); ?>
+      <?php the_content(); ?>
+    <?php endwhile; ?>
+    <?php wp_reset_postdata(); ?>
+  <?php endif; ?>
+
+</main>
+
+<?php get_footer();
