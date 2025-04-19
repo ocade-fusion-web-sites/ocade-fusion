880c5ee22ef4dbe459c6e8119fdfced0f8dfcc17
Valentin Charrier <valentin.charrier@hotmail.fr>
2025-04-19 10:05:38 +0200

    CP GIT

--- functions.php
+++ functions.php
@@ -10,6 +10,8 @@ remove_action('wp_head', 'print_emoji_detection_script', 7);
 remove_action('admin_print_scripts', 'print_emoji_detection_script');
 remove_action('wp_print_styles', 'print_emoji_styles');
 remove_action('admin_print_styles', 'print_emoji_styles');
+add_action('wp_enqueue_scripts', function () { wp_dequeue_style('wp-block-library'); }, 100); // Supprime le CSS de Gutenberg
+
 
 function add_editor_style_file() {
   wp_enqueue_style(
