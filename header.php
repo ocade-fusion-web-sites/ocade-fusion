5387e5a0323487e308c675561afc541eaeb261a8
Valentin Charrier <valentin.charrier@hotmail.fr>
2025-04-23 09:57:05 +0200

    CP GIT

--- header.php
+++ header.php
@@ -102,8 +102,9 @@ $_IS_SOMMARY = $_IS_ARTICLE || $_IS_AUTHOR;
           </ul>
         </li>
         <?php
-        Ocade_Link('Qualité Web Opquast', '/certificat-opquast/');
         Ocade_Link("A propos d'Ocade Fusion", '/a-propos-ocade-fusion/');
+        Ocade_Link("Glossaire Ocade Fusion", '/glossaire/');
+        Ocade_Link('Qualité Web Opquast', '/certificat-opquast/');
         Ocade_Link('Qui est Valentin Charrier ?', '/author/ocade-fusion/');
         Ocade_Link('Plan du site', '/plan-du-site/');
         ?>
