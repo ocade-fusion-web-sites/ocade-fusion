3ab4a26c8871ebcb9823d3bc04062d86db39bf71
Valentin Charrier <valentin.charrier@hotmail.fr>
2025-06-06 13:49:35 +0200

    CP GIT

--- header.php
+++ header.php
@@ -143,9 +143,11 @@ $_IS_SOMMARY = $_IS_ARTICLE || $_IS_AUTHOR;
           <a href="#body" id="sommaire-title-link" title="Sommaire - Haut de page">Sommaire - Haut de page</a>
           <button onclick="document.getElementById('sommaire').setAttribute('aria-expanded', false); document.getElementById('sommaire-button').focus();">Fermer</button>
         </p>
-        <ul class="sommaire-list"></ul> 
+        <ul class="sommaire-list"></ul>
       </nav>
-    <?php endif; ?> 
+    <?php endif; ?>
+    <a href="https://wa.me/33634892265" id="contact-whatsapp-link-header" title="Contact What'sApp" rel="nofollow" target="_blank"></a>
+    <a href="https://discordapp.com/users/1175045336490848306" id="contact-discord-link-header" title="Contact Discord" rel="nofollow" target="_blank"></a>
   </header>
 
   <div id="mobile-footer-placeholder">
